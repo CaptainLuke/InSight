9bae6a6853c87333692ce9a02c939468fe76a980
Luke Selberg <lukerselberg@gmail.com>
2022-02-19 15:38:02 -0600

    navigation

--- InSight/App.js
+++ InSight/App.js
@@ -1,26 +1,70 @@
 import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import FindObject from './FindObject.js'
+import ReadText from './ReadText.js'
 
-export default function App() {
+
+const Stack = createNativeStackNavigator();
+export default function MyStack() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={App}
+          options={({ navigation }) => ({
+            headerLeft: () => <View></View>,
+            headerTitle: () => header(navigation)
+          })
+          }
+        />
+        <Stack.Screen
+          name="Find Object"
+          component={FindObject}
+          options={({ navigation }) => ({
+            headerLeft: () => <View></View>,
+            headerTitle: () => header(navigation)
+          })
+        }
+        />
+        <Stack.Screen
+          name="Read Text"
+          component={ReadText}
+          options={({ navigation }) => ({
+            headerLeft: () => <View></View>,
+            headerTitle: () => header(navigation)
+          })
+          }
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+function App({ navigation }) {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={{ flex: 1, alignSelf: 'flex-start', paddingLeft: 10 }}>
-        <Text style={{ fontWeight: 'bold' }}>InSight</Text>
-      </View>
-
       <View style={{ flex: 10, alignSelf: 'stretch', alignItems: 'center', borderWidth: 2, margin: 10 }}>
         <Text>Home Page</Text>
       </View>
 
       <View style={{ flex: 1, flexDirection: 'row' }}>
-        <TouchableOpacity style={styles.button} onPress={()=>{}}>
+        <TouchableOpacity style={styles.button} onPress={() => { navigation.navigate('Find Object') }}>
           <Text>Find Object</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={()=>{}}>
+        <TouchableOpacity style={styles.button} onPress={() => { navigation.navigate('Read Text') }}>
           <Text>Read Text</Text>
         </TouchableOpacity>
       </View>
     </SafeAreaView >
   );
+}
+
+function header(navigation) {
+  return (
+    <TouchableOpacity onPress={() => { navigation.navigate('Home') }} style={{ flex: 1 }}><Text style={{ fontWeight: 'bold', fontSize: 30 }}>InSight</Text></TouchableOpacity>
+  )
 }
 
 const styles = StyleSheet.create({
--- InSight/FindObject.js
+++ InSight/FindObject.js
@@ -1,40 +1,36 @@
 import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView } from 'react-native';
 
-export default function FindObject() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={{ flex: 1, alignSelf: 'flex-start', paddingLeft: 10 }}>
-        <Text style={{ fontWeight: 'bold' }}>InSight</Text>
-      </View>
+export default function FindObject({ navigation }) {
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={{ flex: 10, alignSelf: 'stretch', alignItems: 'center', borderWidth: 2, margin: 10 }}>
+                <Text>Find Object</Text>
+            </View>
 
-      <View style={{ flex: 10, alignSelf: 'stretch', alignItems: 'center', borderWidth: 2, margin: 10 }}>
-        <Text>Find Object</Text>
-      </View>
-
-      <View style={{ flex: 1, flexDirection: 'row' }}>
-        <TouchableOpacity style={styles.button} onPress={()=>{}}>
-          <Text>Find Object</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={()=>{}}>
-          <Text>Read Text</Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView >
-  );
+            <View style={{ flex: 1, flexDirection: 'row' }}>
+                <TouchableOpacity style={styles.button} onPress={() => { navigation.navigate('Find Object') }}>
+                    <Text>Find Object</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={() => { navigation.navigate('Read Text') }}>
+                    <Text>Read Text</Text>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView >
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  button: {
-    margin: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-    backgroundColor: '#EBEBEB',
-  }
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    button: {
+        margin: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        backgroundColor: '#EBEBEB',
+    }
 });
